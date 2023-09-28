2077dc1bac9fd1c9821575e8f7b7929eb04b77b4
Flavio Risso Sakakibara <fsakakib@amazon.com>
2023-09-28 17:43:30 +0000

    Adding telescope file browser extension and vim-surround

--- init.lua
+++ init.lua
@@ -339,8 +339,7 @@ vim.keymap.set('n', '<leader>fb', ':Telescope file_browser path=%:p:h select_buf
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'markdown', 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript',
-    'vimdoc', 'vim', 'json', 'yaml' },
+  ensure_installed = { 'bash', 'markdown', 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'json', 'yaml' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = true,
